625a6679bc49421805e2f79821fef9ab840b220c
u-shunpei <s.urayama0601@gmail.com>
2022-10-13 21:30:25 +0900

    first

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
-Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
+
 
 Route::get('/', function () {
     return view('welcome');
