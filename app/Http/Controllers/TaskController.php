40911fb208c41382ead13d195abe8101ae8eaed6
urayama <urayama@printpia.co.jp>
2022-10-14 11:19:07 +0900

    second

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Folder;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index(int $id)
+    {
+//        全てのフォルダを取得する
+        $folders = Folder::all();
+
+//        選ばれたフォルダを取得する
+        $current_folder = Folder::find($id);
+
+//        選ばれたフォルダに紐づくタスクを取得する
+        $tasks = $current_folder->tasks()->get(); // ★
+
+        return view('tasks.index', [
+            'folders' => $folders,
+            'current_folder_id' => $id,
+            'tasks' => $tasks,
+        ]);
+    }
+}
