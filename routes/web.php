f7b71ba5cd74818a65ef0fb2bd2a06fb010e71a4
u-shunpei <s.urayama0601@gmail.com>
2022-10-14 17:37:43 +0900

    フォルダ追加機能作成

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\FolderController;
 
 Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
+Route::post('folders/create', [FolderController::class, 'create']);
 
 Route::get('/', function () {
     return view('welcome');
